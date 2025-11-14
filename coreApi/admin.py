08ec35cd838928f56f9a61a9b3666cbfa8408447
Deepak-80085 <deepak.kumaran.m.10@gmail.com>
2025-11-14 17:06:37 +0530

    ongoing tournaments

--- coreApi/admin.py
+++ coreApi/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import *
 
+
+class CustomUserAdmin(BaseUserAdmin):
+    """
+    Custom User admin that handles both password-based and OAuth users.
+    """
+    # Fields to display in the user list
+    list_display = ('username', 'email', 'is_organizer', 'is_staff', 'is_active')
+
+    # Add is_organizer to the fieldsets
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ('Organization', {'fields': ('is_organizer',)}),
+    )
+    
+    # Add is_organizer to add form
+    add_fieldsets = BaseUserAdmin.add_fieldsets + (
+        ('Organization', {'fields': ('is_organizer',)}),
+    ) 
+
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User, CustomUserAdmin)
 admin.site.register(Order)
